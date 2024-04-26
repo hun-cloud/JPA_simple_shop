c60359428554a0bd5301c1ebabb893aa85fa272b
leejaehun <ljh0901@mlsoft.com>
2024-04-26 14:22:57 +0900

    feat : domain 추가

---
+++ src/main/java/com/example/demo/domain/Book.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+}
