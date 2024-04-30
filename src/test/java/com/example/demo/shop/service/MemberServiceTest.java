e60fa6921faf24c6b5b7cb1ea9c7e7555a30155e
JaeHunJaeHun <jhl9838@gmail.com>
2024-05-01 01:53:11 +0900

    feat : 중복 회원 예외 테스트 추가

--- src/test/java/com/example/demo/shop/service/MemberServiceTest.java
+++ src/test/java/com/example/demo/shop/service/MemberServiceTest.java
@@ -24,7 +24,7 @@ class MemberServiceTest{
 
     @DisplayName("회원가입")
     @Test
-    void test() {
+    void 회원가입() {
         // given
         Member member = new Member();
         member.setName("kim");
@@ -33,5 +33,21 @@ class MemberServiceTest{
         // then
         assertThat(member).isEqualTo(memberRepository.findOne(saveId));
     }
+
+    @DisplayName("")
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+        // when
+        memberService.join(member1);
+        // then
+        assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+    }
+
 
 }
